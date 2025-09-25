19ff6197463c58754ad271e9cbf4500dec4d721a
c0ex38 <cagri.ozay@ogrenci.karatay.edu.tr>
2025-09-25 19:05:15 +0300

    Add project files.

---
+++ NetCoreAI.Project1_ApiDemo/Context/ApiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreAI.Project1_ApiDemo.Entities;
+
+namespace NetCoreAI.Project1_ApiDemo.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=CAGRI-DESKTOP;initial catalog=ApiAIDB;integrated security=true;trustservercertificate=true");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+
+    }
+}
---
+++ NetCoreAI.Project2_ApiComsumeUI/Dtos/UpdateCustomerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace NetCoreAI.Project2_ApiComsumeUI.Dtos
+{
+    public class UpdateCustomerDto
+    {
+        public int customerId { get; set; }
+        public string customerName { get; set; }
+        public string customerSurname { get; set; }
+        public decimal customerBalance { get; set; }
+    }
+}
